1573a843a94ef9bd557c359cbdac3e79f056bb32
Thomas.Baumeister <thomasbaumeister3@gmail.com>
2023-08-03 09:46:52 -0500

    Instructions written

--- src/components/Instructions.jsx
+++ src/components/Instructions.jsx
@@ -2,7 +2,13 @@ const Instructions = () => {
     return (
         <>
             <div>
-                <h1>Instructions COmponent</h1>
+                <h1>Instructions for Hangman</h1>
+                    <p>If you dont know how to play hangman, then youve been living under a rock.</p>
+                        <p>Pick a letter, and use that letter to try and guess the word displayed.</p>
+                        <p>If the letter is wrong, it will appear in the graveyard. If it is correct, it will display in place of the blank letter it matches for the word</p>
+                        <p>Be careful, you only get 5 guesses before you will be alerted of your failure and the page will be refreshed</p>
+
+                    
             </div>
 
         </>
